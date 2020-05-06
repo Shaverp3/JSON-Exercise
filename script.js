9aa931acee3e70e0bd2b8f84de73aed95bc466fb
Pat Shaver <pat.shaver@icloud.com>
2020-05-06 16:03:21 -0400

    added function to convert price range to dollar signs

--- script.js
+++ script.js
@@ -59,28 +59,28 @@
                 let restaurantPriceRange = place.restaurant.price_range
                 let restaurantURL = place.restaurant.url
                 let restaurantMenu = place.restaurant.menu_url
-    
-                if (restaurantPriceRange >= 5) {
-                    restaurantPriceRange = "$$$$$"
-                } else {
-                    if (restaurantPriceRange >= 4 && restaurantPriceRange < 5) {
-                        restaurantPriceRange = "$$$$"
-                    } else {
-                        if (restaurantPriceRange >= 3 && restaurantPriceRange < 4) {
-                            restaurantPriceRange = "$$$"
-                        } else {
-                            if (restaurantPriceRange >= 2 && restaurantPriceRange < 3) {
-                                restaurantPriceRange = "$$"
-                            } else {
-                                if (restaurantPriceRange >= 1 && restaurantPriceRange < 2) {
-                                    restaurantPriceRange = "$"
-                                } else {
-                                    restaurantPriceRange = "NA"
-                                }
-                            }
-                        }
-                    }
-                }
+                restaurantPriceRange = convertToDollarSign(`${restaurantPriceRange}`)
+                // if (restaurantPriceRange >= 5) {
+                //     restaurantPriceRange = "$$$$$"
+                // } else {
+                //     if (restaurantPriceRange >= 4 && restaurantPriceRange < 5) {
+                //         restaurantPriceRange = "$$$$"
+                //     } else {
+                //         if (restaurantPriceRange >= 3 && restaurantPriceRange < 4) {
+                //             restaurantPriceRange = "$$$"
+                //         } else {
+                //             if (restaurantPriceRange >= 2 && restaurantPriceRange < 3) {
+                //                 restaurantPriceRange = "$$"
+                //             } else {
+                //                 if (restaurantPriceRange >= 1 && restaurantPriceRange < 2) {
+                //                     restaurantPriceRange = "$"
+                //                 } else {
+                //                     restaurantPriceRange = "NA"
+                //                 }
+                //             }
+                //         }
+                //     }
+                // }
                 console.log(restaurantName, restaurantLocation, restaurantRating, restaurantPriceRange, restaurantURL)
                 document.querySelector("#resultsContainer").innerHTML +=
                     `<div class="card text-center" style="width: 25rem;">
@@ -111,3 +111,42 @@
 
     })
 
+    // Get the input field
+var input = document.getElementById("myInput");
+
+// Execute a function when the user releases a key on the keyboard
+// input.addEventListener("keyup", function(event) {
+//   // Number 13 is the "Enter" key on the keyboard
+//   if (event.keyCode === 13) {
+//     // Cancel the default action, if needed
+//     event.preventDefault();
+//     // Trigger the button element with a click
+//     document.getElementById("myBtn").click();
+//   }
+// });
+
+    //function to convert restaurant price range from number to $$
+
+    const convertToDollarSign = (r) =>{
+        if (r >= 5) {
+            return '$$$$$'
+        } else {
+            if (r >= 4 && r < 5) {
+                return '$$$$'
+            } else {
+                if (r >= 3 && r < 4) {
+                    return '$$$'
+                } else {
+                    if (r >= 2 && r < 3) {
+                        return '$$'
+                    } else {
+                        if (r >= 1 && r < 2) {
+                            return '$'
+                        } else {
+                            return 'NA'
+                        }
+                    }
+                }
+            }
+        }
+    }
