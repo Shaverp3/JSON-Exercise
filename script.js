99c2e9fd31cbcf5fc40a5066f9fc786a17687d7c
Pat Shaver <pat.shaver@icloud.com>
2020-05-06 14:42:33 -0400

    Finished search function

--- script.js
+++ script.js
@@ -1,59 +1,113 @@
-fetch("http://localhost:8088/restaurants")
-    .then(response => response.json())
-    .then(myParsedRestaurants => {
-        myParsedRestaurants.forEach(place => {
-            // console.log(place.restaurant.name)
-            let restaurantName = place.restaurant.name
-            let restaurantLocation = place.restaurant.location.address
-            let restaurantRating = place.restaurant.user_rating.aggregate_rating
-            let restaurantPriceRange = place.restaurant.price_range
-            let restaurantURL = place.restaurant.url
-            let restaurantMenu = place.restaurant.menu_url
+// fetch("http://localhost:8088/restaurants")
+//     .then(response => response.json())
+//     .then(myParsedRestaurants => {
+//         myParsedRestaurants.forEach(place => {
+//             // console.log(place.restaurant.name)
+//             let restaurantName = place.restaurant.name
+//             let restaurantLocation = place.restaurant.location.address
+//             let restaurantRating = place.restaurant.user_rating.aggregate_rating
+//             let restaurantPriceRange = place.restaurant.price_range
+//             let restaurantURL = place.restaurant.url
+//             let restaurantMenu = place.restaurant.menu_url
 
-            if (restaurantPriceRange >= 5) {
-                restaurantPriceRange = "$$$$$"
-            } else {
-                if (restaurantPriceRange >= 4 && restaurantPriceRange < 5) {
-                    restaurantPriceRange = "$$$$"
+//             if (restaurantPriceRange >= 5) {
+//                 restaurantPriceRange = "$$$$$"
+//             } else {
+//                 if (restaurantPriceRange >= 4 && restaurantPriceRange < 5) {
+//                     restaurantPriceRange = "$$$$"
+//                 } else {
+//                     if (restaurantPriceRange >= 3 && restaurantPriceRange < 4) {
+//                         restaurantPriceRange = "$$$"
+//                     } else {
+//                         if (restaurantPriceRange >= 2 && restaurantPriceRange < 3) {
+//                             restaurantPriceRange = "$$"
+//                         } else {
+//                             if (restaurantPriceRange >= 1 && restaurantPriceRange < 2) {
+//                                 restaurantPriceRange = "$"
+//                             } else {
+//                                 restaurantPriceRange = "NA"
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//             console.log(restaurantName, restaurantLocation, restaurantRating, restaurantPriceRange, restaurantURL)
+//             document.querySelector("#resultsContainer").innerHTML +=
+//                 `<div class="card text-center" style="width: 25rem;">
+//                     <div class="card-body">
+//                         <h5 class="card-title"><a href=${restaurantURL}>${restaurantName}</a></h5>
+//                         <p class="card-text">Rating = ${restaurantRating} out of 5 &nbsp;&nbsp;&nbsp;&nbsp;Price:&nbsp;&nbsp;${restaurantPriceRange}</p>
+//                         <a href=${restaurantMenu} class="btn btn-primary">Menu</a>
+//                     </div>
+//                 </div>
+
+//             `
+//             // <li><a href=${restaurantURL}>${restaurantName}</a> - ${restaurantLocation}</li><p>Rating = ${restaurantRating} out of 5 &nbsp;&nbsp;&nbsp;&nbsp;Price:&nbsp;&nbsp;${restaurantPriceRange}`
+
+//         })
+//     })
+
+    function restaurantSearch(searchName) {
+        fetch(`http://localhost:8088/restaurants${searchName}`)
+        .then(response => response.json())
+        .then(myParsedRestaurants => {
+            myParsedRestaurants.forEach(place => {
+                // console.log(place.restaurant.name)
+                let restaurantName = place.restaurant.name
+                let restaurantLocation = place.restaurant.location.address
+                let restaurantRating = place.restaurant.user_rating.aggregate_rating
+                let restaurantPriceRange = place.restaurant.price_range
+                let restaurantURL = place.restaurant.url
+                let restaurantMenu = place.restaurant.menu_url
+    
+                if (restaurantPriceRange >= 5) {
+                    restaurantPriceRange = "$$$$$"
                 } else {
-                    if (restaurantPriceRange >= 3 && restaurantPriceRange < 4) {
-                        restaurantPriceRange = "$$$"
+                    if (restaurantPriceRange >= 4 && restaurantPriceRange < 5) {
+                        restaurantPriceRange = "$$$$"
                     } else {
-                        if (restaurantPriceRange >= 2 && restaurantPriceRange < 3) {
-                            restaurantPriceRange = "$$"
+                        if (restaurantPriceRange >= 3 && restaurantPriceRange < 4) {
+                            restaurantPriceRange = "$$$"
                         } else {
-                            if (restaurantPriceRange >= 1 && restaurantPriceRange < 2) {
-                                restaurantPriceRange = "$"
+                            if (restaurantPriceRange >= 2 && restaurantPriceRange < 3) {
+                                restaurantPriceRange = "$$"
                             } else {
-                                restaurantPriceRange = "NA"
+                                if (restaurantPriceRange >= 1 && restaurantPriceRange < 2) {
+                                    restaurantPriceRange = "$"
+                                } else {
+                                    restaurantPriceRange = "NA"
+                                }
                             }
                         }
                     }
                 }
-            }
-            console.log(restaurantName, restaurantLocation, restaurantRating, restaurantPriceRange, restaurantURL)
-            document.querySelector("#resultsContainer").innerHTML +=
-                `<div class="card text-center" style="width: 25rem;">
-                    <div class="card-body">
-                        <h5 class="card-title"><a href=${restaurantURL}>${restaurantName}</a></h5>
-                        <p class="card-text">Rating = ${restaurantRating} out of 5 &nbsp;&nbsp;&nbsp;&nbsp;Price:&nbsp;&nbsp;${restaurantPriceRange}</p>
-                        <a href=${restaurantMenu} class="btn btn-primary">Menu</a>
+                console.log(restaurantName, restaurantLocation, restaurantRating, restaurantPriceRange, restaurantURL)
+                document.querySelector("#resultsContainer").innerHTML +=
+                    `<div class="card text-center" style="width: 25rem;">
+                        <div class="card-body">
+                            <h5 class="card-title"><a href=${restaurantURL}>${restaurantName}</a></h5>
+                            <p class="card-text">Rating = ${restaurantRating} out of 5 &nbsp;&nbsp;&nbsp;&nbsp;Price:&nbsp;&nbsp;${restaurantPriceRange}</p>
+                            <a href=${restaurantMenu} class="btn btn-primary">Menu</a>
+                        </div>
                     </div>
-                </div>
+    
+                `
+                });
+            }
+            )
+    }
 
-            `
-            // <li><a href=${restaurantURL}>${restaurantName}</a> - ${restaurantLocation}</li><p>Rating = ${restaurantRating} out of 5 &nbsp;&nbsp;&nbsp;&nbsp;Price:&nbsp;&nbsp;${restaurantPriceRange}`
+    document.querySelector("#searchBtn").addEventListener("click", function(){
+        let searchValue = document.querySelector("#searchRestaurants").value
+        document.querySelector("#resultsContainer").innerHTML = ` `
+        
+        if(searchValue != "") {
+            // searchValue=includes(`${searchValue}`)
+            let fetchSearch = `?q=`+ `${searchValue}`
+            restaurantSearch(`${fetchSearch}`)
+        }else{
+            restaurantSearch(``)
+        }
 
-        })
     })
 
-
-
-
-            //     // console.log(brewery.name)
-            // });
-
-
-            // parsedBreweriesArray.forEach(element => {
-            //     console.log(element.name)
-            //     if(element.name)
