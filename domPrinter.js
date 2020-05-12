e24d2ffd9ae2b16c90ed6ef9955905a5acb06b1f
Pat Shaver <pat.shaver@icloud.com>
2020-05-12 10:51:06 -0400

    finished Deleting from database and DOM exercise

--- domPrinter.js
+++ domPrinter.js
@@ -1,13 +1,14 @@
 import convertToDollarSign from "./transform.js"
 
 const buildSingleCard = (eachRestaurant) => {
-    let restaurantPriceRange = convertToDollarSign(eachRestaurant.restaurant.price_range)
+    // let restaurantPriceRange = convertToDollarSign(eachRestaurant.restaurant.price_range)
     return `
     <div class="card text-center" style="width: 25rem;">
     <div class="card-body">
-        <h5 class="card-title"><a href="${eachRestaurant.restaurant.url}" target="_blank">${eachRestaurant.restaurant.name}</a></h5>
-        <p class="card-text">Rating = ${eachRestaurant.restaurant.user_rating.aggregate_rating} out of 5 &nbsp;&nbsp;&nbsp;&nbsp;Price:&nbsp;&nbsp;${restaurantPriceRange}</p>
-        <a href=${eachRestaurant.restaurant.menu_url} class="btn btn-primary">Menu</a>
+        <h5 class="card-title"><a href="${eachRestaurant.url}" target="_blank">${eachRestaurant.name}</a></h5>        <p class="card-text">${eachRestaurant.address}</p>
+        <p class="card-text">Rating = ${eachRestaurant.averageUserRating} out of 5 &nbsp;&nbsp;Avg. Cost per 2:&nbsp;$${eachRestaurant.averageCostPerTwo}</p>
+        <a href=${eachRestaurant.menuURL} class="btn btn-primary">Menu</a>
+        <a id="delete-btn-${eachRestaurant.id}" class="btn btn-primary">Delete</a>
     </div>
     </div>
 `
--- script.js
+++ script.js
@@ -5,8 +5,8 @@ import restaurantSearch from "./eventListeners.js"
 //Prints all restaurants in the database upon page load
 apiManagerObject.getAllRestaurantsFromAPI()
     .then((myParsedRestaurants) => {
-         printAllRestaurants(myParsedRestaurants)
-});
+        printAllRestaurants(myParsedRestaurants)
+    });
 
 //code for listening for click event on search button
 document.querySelector("#searchBtn").addEventListener("click", restaurantSearch);
@@ -15,5 +15,59 @@ document.querySelector("#searchBtn").addEventListener("click", restaurantSearch)
 document.querySelector("#searchRestaurants").addEventListener("keyup", function () {
     if (event.keyCode === 13) {
         document.querySelector("#searchBtn").click();
+    }
+});
+
+//Gets input from user and adds new restaurant (post) to restaurants.json database
+
+document.querySelector("#newRestaurantBtn").addEventListener("click", function () {
+    let restUrl = document.querySelector("#restaurant-url").value
+    let restAddress = document.querySelector("#restaurant-address").value
+    let restMenu = document.querySelector("#restaurant-menuURL").value
+    let restName = document.querySelector("#restaurant-name").value
+    let restCost = document.querySelector("#average-cost").value
+    let restRating = document.querySelector("#restaurant-rating").value
+    // console.log(restName,restAddress,restUrl,restMenu,restCost,restRating)
+
+    const restaurantObject = {
+        url: restUrl,
+        menuURL: restMenu,
+        name: restName,
+        averageUserRating: restRating,
+        averageCostPerTwo: restCost,
+        address: restAddress
+    }
+
+    fetch("http://localhost:8088/restaurants", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(restaurantObject)
+    }).then(function () {
+        document.querySelector("#resultsContainer").innerHTML = ""
+        apiManagerObject.getAllRestaurantsFromAPI()
+            .then((myParsedRestaurants) => {
+                printAllRestaurants(myParsedRestaurants)
+            })
+    });
+
+    console.log(restaurantObject)
+});
+
+document.querySelector("body").addEventListener("click", () => {
+    if (event.target.id.includes("delete-btn")) {
+        // console.log(event.target.id)
+        // console.log(event.target.id.split("-")[2])
+        const primaryKey = event.target.id.split("-")[2];
+        fetch(`http://localhost:8088/restaurants/${primaryKey}`, {
+            method: "DELETE",
+        }).then(() => {
+            document.querySelector("#resultsContainer").innerHTML = "";
+            apiManagerObject.getAllRestaurantsFromAPI()
+                .then((myParsedRestaurants) => {
+                    printAllRestaurants(myParsedRestaurants);
+                });
+        });
     }
 });
